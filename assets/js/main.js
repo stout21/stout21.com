347fbb7310d5870a381645b83394bb878ffdc4e5
Dustan Kasten <dustan.kasten@gmail.com>
2013-04-18 23:03:05 -0400

    Start adding videos and hooking up some basic events for it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,45 @@
-(function (skrollr) {
+(function (skrollr, _V_) {
 
   var skrl = skrollr.init();
+  var player = _V_('the-video');
+  console.log(player);
 
   window.skrl = skrl;
 
-})(window.skrollr);
+  var $w = $(window).on('resize', onResize),
+      w, h;
+
+  $('a[href=#play-video]').on('click', function(e) {
+    e.preventDefault();
+    player.play();
+  });
+
+  function onResize() {
+    w = $w.width();
+    h = $w.height();
+    setDimensions();
+  }
+
+  player.ready(function () {
+    //player = this;
+  });
+
+  player.addEvent('play', function () {
+    $('#video')
+      .removeClass('isPaused')
+      .addClass('isPlaying');
+  });
+  player.addEvent('pause', function () {
+    $('#video')
+      .removeClass('isPlaying')
+      .addClass('isPaused');
+  });
+
+
+  function setDimensions() {
+    $('#video').height(h);
+  }
+
+  onResize();
+
+})(window.skrollr, window._V_);
