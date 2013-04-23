ee0153849f1a79f1bb082f2e7f5427928d13964e
Dustan Kasten <dustan.kasten@gmail.com>
2013-04-22 23:45:11 -0400

    Basic video event and style hookup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 (function (skrollr, _V_) {
   var skrl;
+  var $w = $(window).on('resize', onResize), w, h;
+  var player = _V_('the-video');
 
   skrollr.menu.init(skrl, {
     animate: true,
@@ -7,27 +9,10 @@
     easing: 'swing'
   });
 
-  $(function() {
-    var CONSTANTS = {
-      applePie: $('.flavorApplePie').offset().top,
-      margarita: $('.flavorMargarita').offset().top,
-      royalFlush: $('.flavorRoyalFlush').offset().top,
-      screwdriver: $('.flavorScrewdriver').offset().top
-    };
-    console.log(CONSTANTS);
-
-    skrl = skrollr.init({
-      constants: CONSTANTS
-    });
-  });
-
-  var player = _V_('the-video');
   console.log(player);
 
   window.skrl = skrl;
 
-  var $w = $(window).on('resize', onResize),
-      w, h;
 
   $('a[href=#play-video]').on('click', function(e) {
     e.preventDefault();
@@ -40,14 +25,29 @@
     setDimensions();
   }
 
+  function onScroll() {
+    console.log('scroll handler');
+    var offsetTop = $('#about').offset().top;
+    var currentScroll = $w.scrollTop();
+    if (currentScroll >= offsetTop) {
+      console.log('stop that video');
+      player.pause();
+      $w.off('scroll');
+    }
+  }
+
   player.ready(function () {
     //player = this;
   });
 
   player.addEvent('play', function () {
+    console.log('play event');
+
     $('#video')
       .removeClass('isPaused')
       .addClass('isPlaying');
+
+    $w.on('scroll', throttle(onScroll, 100));
   });
   player.addEvent('pause', function () {
     $('#video')
@@ -55,11 +55,36 @@
       .addClass('isPaused');
   });
 
-
   function setDimensions() {
     $('#video').height(h);
   }
 
-  onResize();
+  function throttle(fn, time) {
+    var timeout;
+    return function () {
+      if (timeout) return;
+
+      timeout = setTimeout(function () {
+        fn();
+        timeout = null;
+      }, time);
+    };
+  }
+
+
+  $(function() {
+    var CONSTANTS = {
+      applePie: $('.flavorApplePie').offset().top,
+      margarita: $('.flavorMargarita').offset().top,
+      royalFlush: $('.flavorRoyalFlush').offset().top,
+      screwdriver: $('.flavorScrewdriver').offset().top
+    };
+
+    skrl = skrollr.init({
+      constants: CONSTANTS
+    });
+
+    onResize();
+  });
 
 })(window.skrollr, window._V_);
